d99c6a494cdebd7ae50776b84a2dd2abe1dfe04c
DoctorDraxter <doctordraxter@gmail.com>
2022-04-04 22:37:18 -0400

    folder structure changed

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,3 +1,13 @@
+import { Header } from './overviewSections/Header';
+import { NFTSOverview } from './overviewSections/NFTSOverview';
+import { OverviewDescription } from './overviewSections/OverviewDescription';
+
 export const Overview = () => {
-	return <main>Works</main>;
+	return (
+		<main>
+			<Header />
+			<OverviewDescription />
+			<NFTSOverview />
+		</main>
+	);
 };
--- src/pages/overviewSections/components/NFTOverview.jsx
+++ src/pages/overviewSections/components/NFTOverview.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { variables } from '../../../../shared/GlobalStyles';
+import { variables } from '../../../shared/GlobalStyles';
 
 export const NFTOverview = ({
 	img,
