1f15191e38ac08b89f337ea728c2767d434a9c8d
DoctorDraxter <doctordraxter@gmail.com>
2022-04-04 22:05:01 -0400

    Change Overview file to Family

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { HomePage } from './pages/Home';
-import { OverviewPage } from './pages/overview/Overview';
+import { OverviewPage } from './pages/overview/Family';
 import { Navigation } from './shared/Navigation';
 import { Footer } from './shared/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
