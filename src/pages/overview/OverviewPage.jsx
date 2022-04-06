1614e8e07c9335d3fda3e46f0b4790546ccc2425
DoctorDraxter <doctordraxter@gmail.com>
2022-04-05 21:04:53 -0400

    Overview page changed

--- src/pages/overview/OverviewPage.jsx
+++ src/pages/overview/OverviewPage.jsx
@@ -2,7 +2,7 @@ import { Benefits } from './sections/Benefits';
 import { Header } from './sections/Header';
 import { NFTSOverview } from './sections/NFTSOverview';
 import { NFTSRoi } from './sections/NFTSRoi';
-import { OverviewDescription } from '../overviewSections/OverviewDescription';
+import { OverviewDescription } from './sections/OverviewDescription';
 import { Roadmap } from './sections/Roadmap';
 
 export const Overview = () => {
